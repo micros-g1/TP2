4febbec1f1f7266858e5530c6a2269c9d3469ae0
taomasgonzalez <togonzalez@itba.edu.ar>
2019-09-28 21:19:14 -0300

    Documentation with doxygen finished.

--- source/I2C/i2c_master_dr.c
+++ source/I2C/i2c_master_dr.c
@@ -7,17 +7,23 @@
 
 #include <I2C/i2c_master_dr.h>
 #include "MK64F12.h"
-
+#include "stdlib.h"
 
 #define I2C_CLK_FREQ	50000000
 
+i2c_service_callback_t interruption_callback = NULL;
 
-void i2c_master_init(){
+void i2c_dr_master_init(i2c_service_callback_t callback){
 	SIM->SCGC4 |= SIM_SCGC4_I2C0(1);		//clock gating, feed clock to the module
 	I2C_Type* i2c0_pos = (I2C_Type*) I2C0_BASE;
 	//clock module is set to 50Mhz!!
 	i2c0_pos->F = 0x2D;		//set baud rate to 78125Hz. mult == 0x0-> mult=1; scl div ==2D-> scl div=640; !!!
 	i2c0_pos->C1 = 0xF8;	//IICEN = 1; IICIE = 1; MST = 1; TX = 1; TXAK = 1; RSTA = 0; WUEN = 0; DMAEN = 0.
+	interruption_callback = callback;
+}
+
+void I2C0_IRQHandler(){
+	interruption_callback();
 }
 
 /**************************************
@@ -26,48 +32,52 @@ void i2c_master_init(){
 *7		6		5	4	3		2	1		0
 *TCF, IAAS, BUSY, ARBL, RAM, SRW, IICIF, RXAK
 */
-/*TCF
- * Acknowledges a byte transfer; TCF is set on the completion of a byte transfer. This bit is valid only during
-or immediately following a transfer to or from the I2C module. TCF is cleared by reading the I2C data
-register in receive mode or by writing to the I2C data register in transmit mode.
-0 Transfer in progress
-1 Transfer complete
- */
-bool get_transfer_complete(){
+
+bool i2c_dr_get_transfer_complete(){
+	/*TCF
+	 * Acknowledges a byte transfer; TCF is set on the completion of a byte transfer. This bit is valid only during
+	or immediately following a transfer to or from the I2C module. TCF is cleared by reading the I2C data
+	register in receive mode or by writing to the I2C data register in transmit mode.
+	0 Transfer in progress
+	1 Transfer complete
+	 */
 	return ((((I2C_Type*) I2C0_BASE)->S) >> 7) & 1U;
 }
 
 /*IAAS : for slave, not necessary!!!*/
 
-//BUSY: 1 for bus busy.
-bool bus_is_busy(){
+bool i2c_dr_bus_is_busy(){
+	//BUSY: 1 for bus busy. 0 for bus not busy
 	return ((I2C_Type*) I2C0_BASE)->S |= 0x10;
 }
+
 // ARBL -> ARBITRATION LOST. NOT NECESSARY BECAUSE THE TRANSMISSION WILL ONLY BE FROM MASTER (MCU) TO SLAVE(MAGNETOMETER), READ AND WRITE.
 //RAM -> RANGE ADDRESS MATCH.
 // SRW -> FOR SLAVE ONLY!!
-/* IICIF
- * This bit sets when an interrupt is pending. This bit must be cleared by software by writing 1 to it, such as in
-the interrupt routine. One of the following events can set this bit:
 
-• One byte transfer, including ACK/NACK bit, completes if FACK is 0. An ACK or NACK is sent on the
-bus by writing 0 or 1 to TXAK after this bit is set in receive mode.
-• One byte transfer, excluding ACK/NACK bit, completes if FACK is 1.
-• Match of slave address to calling address including primary slave address, range slave address,
-alert response address, second slave address, or general call address.
-• Arbitration lost
-• In SMBus mode, any timeouts except SCL and SDA high timeouts
-• I2C bus stop or start detection if the SSIE bit in the Input Glitch Filter register is 1
-*/
-void clear_iicif(){
+void i2c_dr_clear_iicif(){
+	/* IICIF -- see i2c_dr_get_iicif also for more information
+	 * This bit sets when an interrupt is pending. This bit must be cleared by software by writing 1 to it, such as in
+	 * the interrupt routine.
+	*/
 	((I2C_Type*) I2C0_BASE)->S &= ~(1UL << 1);
 }
-bool get_iicif(){
+bool i2c_dr_get_iicif(){
+	/* IICIF -- see i2c_dr_clear_iicif also for more information
+	 * This bit sets when an interrupt is pending. One of the following events can set this bit:
+	 * One byte transfer, including ACK/NACK bit, completes if FACK is 0. An ACK or NACK is sent on the
+	bus by writing 0 or 1 to TXAK after this bit is set in receive mode.
+	 * One byte transfer, excluding ACK/NACK bit, completes if FACK is 1.
+	 * Match of slave address to calling address including primary slave address, range slave address,
+	alert response address, second slave address, or general call address.
+	 * Arbitration lost
+	 * In SMBus mode, any timeouts except SCL and SDA high timeouts
+	 * I2C bus stop or start detection if the SSIE bit in the Input Glitch Filter register is 1
+	*/
 	return ((((I2C_Type*) I2C0_BASE)->S) >> 1) & 1U;
 }
 
-//RXAK -> Receive Acknowledge. 0: Acknowledge signal detected after the completion of one byte of data transmission on the bus.
-bool get_rxak(){
+bool i2c_dr_get_rxak(){
 	return (((I2C_Type*) I2C0_BASE)->S) & 1U;
 }
 
@@ -80,10 +90,10 @@ of data.
 NOTE: When making the transition out of master receive mode, switch the I2C mode before reading the
 Data register to prevent an inadvertent initiation of a master receive data transfer.
 */
-void write_data(unsigned char data){
+void i2c_dr_write_data(unsigned char data){
 	((I2C_Type*) I2C0_BASE)->D = data;
 }
-unsigned char read_data(){
+unsigned char i2c_dr_read_data(){
 	return ((I2C_Type*) I2C0_BASE)->D;
 }
 
@@ -97,30 +107,30 @@ unsigned char read_data(){
 
 //STOPF ->FOR SLAVES ONLY!!!
 
-/*SSIE
-* I2C Bus Stop or Start Interrupt Enable
-* This bit enables the interrupt for I2C bus stop or start detection.
-* NOTE: To clear the I2C bus stop or start detection interrupt: In the interrupt service routine, first clear the
-* 		STOPF or STARTF bit by writing 1 to it, and then clear the IICIF bit in the status register.
-* 		If this sequence is reversed, the IICIF bit is asserted again.
-* 		1 Stop or start detection interrupt is enabled
-* 		0 Stop or start detection interrupt is disabled
- */
-void set_start_stop_interrupt(bool enabled){
+void i2c_dr_set_start_stop_interrupt(bool enabled){
+
+	/*SSIE -- I2C Bus Stop or Start Interrupt Enable
+	* This bit enables the interrupt for I2C bus stop or start detection.
+	* 		1 Stop or start detection interrupt is enabled
+	* 		0 Stop or start detection interrupt is disabled
+	 */
 	unsigned char reg = ((I2C_Type*) I2C0_BASE)->FLT;
 	(((I2C_Type*) I2C0_BASE)->FLT) ^= (-(unsigned char)enabled ^ reg) & (1UL << 5);
 }
-/*STARTF
- * Hardware sets this bit when the I2C bus's start status is detected. The STARTF bit must be cleared by
-writing 1 to it.
-0 No start happens on I2C bus
-1 Start detected on I2C bus
- */
-bool get_startf(){
+
+bool i2c_dr_get_startf(){
+	/*STARTF -- see i2c_dr_get_startf for more information
+	 * Hardware sets this bit when the I2C bus's start status is detected.
+	 * 0 No start happens on I2C bus
+	 * 1 Start detected on I2C bus
+	 */
 	return ((((I2C_Type*) I2C0_BASE)->FLT) >> 4) & 1U;
 }
 
-void clear_startf(){
+void i2c_dr_clear_startf(){
+	/*STARTF -- see i2c_dr_get_startf for more information
+	*The STARTF bit must be cleared by writing 1 to it.
+	*/
 	(((I2C_Type*) I2C0_BASE)->FLT) |= 1UL << 4;
 }
 
--- source/I2C/i2c_master_dr.h
+++ source/I2C/i2c_master_dr.h
@@ -1,31 +1,108 @@
-/*
- * i2c_master.h
- *
- *  Created on: 27 sep. 2019
- *      Author: Tomas
- */
-/*
- * This file is prepared to
+/**
+ * @file i2c_master_dr.h
+ * @author Grupo 1 Labo de Micros
+ * @date 24 Sep 2019
+ * @brief I2C Master Driver
+ * @details
+ * I2C Driver
+ * This driver is prepared to
  * handle the communication with the magnetometer of the freedom board
- * there are many unhandled cases (this is not a generic i2c master driver!!)
+ * there are many unhandled cases (this is not a generic i2c master driver as it does not handle multimaster!!)
+ * Although not all multimaster cases are handled, some of them may be implemented for future upgrades/implementations.
  */
 
 #ifndef I2C_I2C_MASTER_DR_H_
 #define I2C_I2C_MASTER_DR_H_
 #include <stdbool.h>
 
-void i2c_master_init();
+/**
+ * @typedef void (*i2c_service_callback_t)(void)
+ * @brief I2C callback to be called whenever a hardware interrupt related to
+ * the I2C master driver is called. See also i2c_dr_master_init().
+ */
+typedef void (*i2c_service_callback_t)(void);
 
-bool get_transfer_complete();
-bool bus_is_busy();
-void clear_iicif();
-bool get_iicif();
-bool get_rxak();
-void write_data(unsigned char data);
-unsigned char read_data();
-void set_start_stop_interrupt(bool enabled);
-bool get_startf();
-void clear_startf();
+/**
+ * @brief I2C initialize master mode register configuration.
+ * @details Initializes master internal configuration on the i2c module.
+ * Sets a callback to be called whenever a hardware interrupt that is related
+ * to this master is called.
+ * Check the MK64 user manual (Table 51-5) for information on what hardware interrupts would be called
+ * and the status, flag and enable they signify.
+ * Uses the I2c0 module.
+ * @param callback : callback to be called when a hardware interrupt is called.
+ */
+void i2c_dr_master_init(i2c_service_callback_t callback);
+/**
+ * @brief I2C Get Data Transfer Complete
+ * @details Gets the current status of the last data transfer.
+ * This call is valid only during or immediately following a transfer to or from the I2C module.
+ * The Transfer status is cleared by reading the I2C data register in receive mode
+ * or by writing to the I2C data register in transmit mode.
+ * @return *false* when the last data transfer is still in progress. *true* when completed.
+ */
+bool i2c_dr_get_transfer_complete();
+/**
+ * @brief I2C Get the bus status
+ * @details I2C Gets the bus status: Busy or not.
+ * The bus may be busy when this or some other master is taking over it
+ * (a START sequence has been sent but no STOP sequence has arrived yet).
+ * @return *true* when an interrupt is pending, *false* otherwise.
+ */
+bool i2c_dr_bus_is_busy();
+/**
+ * @brief I2C Get the interrupt flag status
+ * @details I2C Gets the interrupt status: pending or already handled.
+ * If the flag is on, then a pending interrupt should be handled.
+ * @return *true* when an interrupt is pending, *false* otherwise.
+ */
+bool i2c_dr_get_iicif();
+/**
+ * @brief I2C Clear the interrupt flag status
+ * @details Should be called when handling an I2C master interrupt. See also i2c_dr_get_iicif().
+ */
+void i2c_dr_clear_iicif();
+/**
+ * @brief I2C Get the received ACK signal.
+ * @details I2C Gets the received ACK signal from the slave.
+ * If the ACK signal shows if there was a problem in the communication and
+ * whether the slave is able to receive more information or not.
+ * @return *false* if the ACK showed no problem, *true* otherwise.
+ */
+bool i2c_dr_get_rxak();
+
+/**
+ * @brief I2C Write data to the bus.
+ * @details writes an entire byte to the bus buffer.
+ */
+void i2c_dr_write_data(unsigned char data);
+/**
+ * @brief I2C Read data from the bus.
+ * @details Reads an entire byte from the bus buffer (sent from slave to this master).
+ * Clears the Transfer status when called in receive mode. See also i2c_dr_get_transfer_complete()
+ * @return the byte that was read.
+ */
+unsigned char i2c_dr_read_data();
+/**
+ * @brief I2C Enable or Disable Start and Stop Interrupts
+ * @details Disables or enables the start or stop detection interrupts.
+ * NOTE: To clear the I2C bus stop or start detection interrupt: In the interrupt service routine, first call
+ * i2c_dr_clear_startf, and then call i2c_dr_clear_iicif.
+ * If this sequence is reversed, the IICIF flag is asserted again.
+ * @param enabled : true when the start stop interrupts should be enabled. False otherwise.
+ */
+void i2c_dr_set_start_stop_interrupt(bool enabled);
+/**
+ * @brief I2C Get the current status of the start interrupt flag
+ * @details I2C Gets the current status of the start interrupt flag.
+ * @return *true* if the flag is set, *false* otherwise.
+ */
+bool i2c_dr_get_startf();
+/**
+ * @brief I2C clear start interrupt flag
+ * @details Clears the startf interrupt flag, see also i2c_dr_get_startf() .
+ */
+void i2c_dr_clear_startf();
 
 
 #endif /* I2C_I2C_MASTER_DR_H_ */
