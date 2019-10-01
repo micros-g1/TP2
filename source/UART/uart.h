c5167b87d175d2b7e4f786bebe1bc8e86cac44cf
rocioparra <rparra@itba.edu.ar>
2019-10-01 09:17:04 -0300

    uart driver working, added systick and queues

---
+++ source/UART/uart.h
@@ -0,0 +1,109 @@
+/***************************************************************************//**
+  @file     UART.c
+  @brief    UART Driver for K64F. Non-Blocking and using FIFO feature
+  @author   Nicolás Magliola
+ ******************************************************************************/
+
+#ifndef _UART_H_
+#define _UART_H_
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "MK64F12.h"
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+#define UART_N_IDS   5
+
+//#define UART_HAL_DEFAULT_BAUDRATE 4800
+
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+typedef struct {
+	uint8_t parity : 1; 		// true for using parity bit
+	uint8_t odd_parity : 1;		// true for odd parity (if parity is true)
+	uint8_t eight_bit_word : 1;	// true for eight bit words, false for nine
+	uint8_t interrupts : 1;		// true to enable non blocking functionality
+	uint32_t baudrate;
+} uart_cfg_t;
+
+
+/*******************************************************************************
+ * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/**
+ * @brief Initialize UART driver
+ * @param id UART's number
+ * @param config UART's configuration (baudrate, parity, etc.)
+*/
+void uartInit (uint8_t id, uart_cfg_t config);
+
+/**
+ * @brief Check if a new byte was received
+ * @param id UART's number
+ * @return A new byte has being received
+*/
+bool uartIsRxMsg(uint8_t id);
+
+/**
+ * @brief Check how many bytes were received
+ * @param id UART's number
+ * @return Quantity of received bytes
+*/
+uint8_t uartGetRxMsgLength(uint8_t id);
+
+/**
+ * @brief Read a received message. Non-Blocking
+ * @param id UART's number
+ * @param msg Buffer to paste the received bytes
+ * @param cant Desired quantity of bytes to be pasted
+ * @return Real quantity of pasted bytes
+*/
+uint8_t uartReadMsg(uint8_t id, char* msg, uint8_t cant);
+
+/**
+ * @brief Write a message to be transmitted. Non-Blocking
+ * @param id UART's number
+ * @param msg Buffer with the bytes to be transferred
+ * @param cant Desired quantity of bytes to be transferred
+ * @return Real quantity of bytes to be transferred
+*/
+uint8_t uartWriteMsg(uint8_t id, const char* msg, uint8_t cant);
+
+/**
+ * @brief Check if all bytes were transfered
+ * @param id UART's number
+ * @return All bytes were transfered
+*/
+bool uartIsTxMsgComplete(uint8_t id);
+
+
+
+////////////////////////
+// BLOCKING FUNCTIONS //
+////////////////////////
+void uart_putchar (UART_Type * channel, char ch);
+char uart_getchar (UART_Type * channel);
+int uart_getchar_present (UART_Type * channel);
+
+
+
+
+/*******************************************************************************
+ ******************************************************************************/
+
+#endif // _UART_H_
---
+++ source/util/SysTick.h
@@ -0,0 +1,97 @@
+/***************************************************************************//**
+  @file     SysTick.h
+  @brief    SysTick driver
+  @author   Nicol�s Magliola
+ ******************************************************************************/
+
+#ifndef _SYSTICK_H_
+#define _SYSTICK_H_
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+#include "general.h"
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+#define SYSTICK_ISR_FREQUENCY_HZ 8000U
+//maximum amout of callbacks that can be added to sysTick
+#define MAX_N_ST_CALLBACKS 20
+
+#define SYSTICK_HZ_TO_RELOAD(x) ( SYSTICK_ISR_FREQUENCY_HZ/(x) - 1 )
+// if 0, then it will be called with the same freq as systick
+// dont forget to check that systick isr frequency is a multiple of the one
+// you wish to implement!
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+typedef void (*systick_callback_t)(void);
+typedef enum{SINGLE_SHOT, PERIODIC} callback_conf_t;
+
+/*******************************************************************************
+ * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/**
+ * @brief Initialize SysTic driver.
+ * The function has no effect when called twice (safe init).
+ * @return void.
+ */
+void systick_init();
+
+/**
+ * @brief Add function to be called on systick interrupts. Enabled by default
+ * @param callback : function to be called when sysTick is called 'reload' times.
+ * @param reload : amount of times sysTick has to tick for the callback to be called.s
+ * @param set_up : configuration option for the callback.
+ */
+void systick_add_callback(systick_callback_t callback, unsigned int reload, callback_conf_t set_up);
+
+/**
+ * @brief Enable callback so that it may be called when the reload limit is reached.
+ * @param callback: callback to be enabled.
+ * @return void.
+ */
+void systick_enable_callback(systick_callback_t callback);
+/**
+ * @brief Disable callback so that it wont be called when the reload limit is reached.
+ * @param callback: callback to be disabled.
+ * @return void.
+ */
+void systick_disable_callback(systick_callback_t callback);
+
+/**
+ * @brief deletes a specific callback from those added with systick_add_callback function
+ * @param callback: callback to be deleted.
+ * @return void.
+ */
+void systick_delete_callback(systick_callback_t callback);
+
+/**
+ * @brief Inform the user if a specific callback has been added (and not deleted after that) to sysTick
+ * @param callback: callback to check.
+ * @return true if the callback was added to sysTick.
+ */
+bool systick_has_callback(systick_callback_t callback);
+/**
+ * @brief get a specific callaback's configuration.
+ * @param callback: callback to get the configuration from.
+ * @return current configuration for the callback.
+ */
+callback_conf_t systick_get_callback_conf(systick_callback_t callback);
+/**
+ * @brief set a specific callback's configuration.
+ * @param callback: callback for which the sysTick configuration will be set.
+ * @param conf: configuration for the callback
+ * @return current configuration for the callback.
+ */
+void systick_set_callback_conf(systick_callback_t callback, callback_conf_t conf);
+/*******************************************************************************
+ ******************************************************************************/
+
+#endif // _SYSTICK_H_
